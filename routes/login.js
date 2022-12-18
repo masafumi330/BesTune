5b990147558004801945fbf6d1860e6513fa1e21
Masafumi Misawa <m.masafumi@wano.co.jp>
2022-12-18 20:31:54 +0900

    :zap: spotify-web-api-nodeモジュールを新しく追加、/loginを実装

---
+++ routes/login.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var spotifyWebApi = require('spotify-web-api-node');
+var config = require('../config');
+
+
+// credentials are optional
+var spotifyApi = new spotifyWebApi({
+  clientId: config.clientId,
+  clientSecret: config.clientSecret,
+  redirectUri: config.redirectUri
+});
+
+var state = 'some-state-of-my-choice';
+var scopes = ['user-read-private', 'user-read-email'];
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+// /* GET /login page. */
+router.get('/', function (req, res, next) {
+  res.redirect(authorizeURL);
+});
+
+module.exports = router;
