f5f25d3543e988e1425b68034913c9e699f90953
Masafumi Misawa <m.masafumi@wano.co.jp>
2022-12-19 17:43:28 +0900

    :wrench: "sign in with spotify"にして/tracksに直接遷移に変更

--- routes/toptracks.js
+++ routes/toptracks.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-var callback = require('./callback.js');
-//Import Module
 const localStorage = require("localStorage");
-const { token } = require('morgan');
 
-//Setting localStorage Item
-localStorage.setItem('accessToken', 'testtoken')
 
 /* Get User's Top Items */
 // /me/top/tracks/?limit=30&time_range=short_term
@@ -19,33 +14,14 @@ localStorage.setItem('accessToken', 'testtoken')
 const getAccessToken = () => localStorage.getItem('accessToken');
 
 router.get('/', function (req, res, next) {
-    // const token = callback.access_token;
-    // const token = getAccessToken();
-    // var options = {
-    //     url: 'https://api.spotify.com/v1/me/top/tracks',
-    //     method: 'GET',
-    //     headers: {
-    //         'Authorization': 'Bearer ' + token,
-    //         'Content-Type': 'application/json'
-    //     },
-    //     qs: {
-    //         'limit': 30,
-    //         'term_range': 'short_term'
-    //     }
-    // };
+    const token = getAccessToken();
+    if (!token) {
+        console.log("tokenがありません！");
+        // auhorize -> exchange token from code
+        res.redirect('/')
+    }
 
-    // request(options, function (error, response, body) {
-    //     console.log(error);
-    //     console.log(response);
-    //     console.log(body);
-    //     res.render('mypage', { json: body });
-    // });
-    console.log("here is /toptracks");
-    console.log(callback.token);
-    var token = callback.token;
-    console.log("here is /toptracks");
-
-    res.render('toptracks', { token: token });
+    res.render('toptracks',);
 });
 
 module.exports = router;
