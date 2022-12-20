de8f5f0580712cf40569d1f2f0866c50129e4c91
Masafumi Misawa <m.masafumi@wano.co.jp>
2022-12-20 12:50:35 +0900

    :zap: loginしたら/mypageにいくよう変更

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const localStorage = require("localStorage");
+const request = require('request');
+
+const getAccessToken = () => localStorage.getItem('accessToken');
+
 /* GET mypage listing. */
 router.get('/', function (req, res, next) {
-  res.render('mypage');
+  const accessToken = getAccessToken();
+  var authOptions = {
+    method: 'GET',
+    url: 'https://api.spotify.com/v1/me/top/tracks',
+    qs: {
+      limit: 30,
+      time_range: "short_term"
+    },
+    headers: {
+      'Authorization': 'Bearer ' + accessToken,
+      'Content-Type': 'application/json'
+    },
+    json: true
+  };
+  request(authOptions, function (error, response, body) {
+    var songList = body.items;
+    res.render('mypage', { songList: songList });
+  })
 });
 
 module.exports = router;
