4f2f4e7a26d901df0d38e094e2410574a6078dcc
Masafumi Misawa <m.masafumi@wano.co.jp>
2022-12-21 12:51:42 +0900

    :zap: css, jsの読み込みができるように

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// 静的ファイルは無条件に公開
+app.use('/public', express.static('public'))
 
 app.use('/', indexRouter);
 app.use('/mypage', mypageRouter);
