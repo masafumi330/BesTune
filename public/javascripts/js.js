372f3a88ea20e3b0427fa99fb809604ce70452ff
Masafumi Misawa <m.masafumi@wano.co.jp>
2022-12-21 21:38:33 +0900

    :zap: Mypageにも幾何学模様追加

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -52,7 +52,7 @@ particlesJS("particles-js", {
         },
         "move": {
             "enable": true,
-            "speed": 6,//この数値を小さくするとゆっくりな動きになる
+            "speed": 3,//この数値を小さくするとゆっくりな動きになる
             "direction": "none",//方向指定なし
             "random": false,//動きはランダムにしない
             "straight": false,//動きをとどめない
